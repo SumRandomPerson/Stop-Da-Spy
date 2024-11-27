b158fdb1b3c9a258b646623f7cfcdf560f2d0df7
Flood99 <113184223+Flood99@users.noreply.github.com>
2024-11-27 13:40:09 -0500

    done

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,16 +7,29 @@ public class EnemyAI : MonoBehaviour
 {
     private NavMeshAgent enemyAgent;
     private GameObject player;
+    private int hp = 10;
     // Start is called before the first frame update
     void Start()
     {
-        enemyAgent = GetComponent<NavMeshAgent>();
-        player = GameObject.Find("Player");
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
+        player = GameObject.Find("Player");
         enemyAgent.destination = player.transform.position;
+        if(hp <=0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.CompareTag("Projectile"))
+        {
+            Debug.Log("hit");
+            Projectile projectileScript = collider.GetComponent<Projectile>();
+            hp -= projectileScript.GetDamage();
+        }
     }
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    private int damage = 2;
     // Start is called before the first frame update
     void Start()
     {
+
+        transform.Translate(Vector3.forward*15 * Time.deltaTime);
         
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(Vector3.forward*15 * Time.deltaTime);
+    public int GetDamage()
+    {   
+        return damage;
+        Destroy(gameObject);
+        
     }
 }
