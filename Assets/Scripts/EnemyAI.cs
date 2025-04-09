488dd0b123583bc5a4887073469be9b502384a16
Flood99 <113184223+Flood99@users.noreply.github.com>
2025-04-09 03:44:48 -0400

    timeStop

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -9,32 +9,43 @@ public class EnemyAI : MonoBehaviour
     private GameObject player;
     private int hp = 10;
     private Animator anim;
+    private TimeManager time;
     // Start is called before the first frame update
     void Start()
     {
+        
         enemyAgent = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
         anim.Play("Movement.Walk");
+        time = GameObject.Find("Game Manager").GetComponent<TimeManager>();
     }
 
     void Update()
     {
-        Debug.Log(enemyAgent.velocity);
-        if(enemyAgent.velocity.x != 0)
+        if(time.isStopped == false)
         {
-            anim.SetBool("Static_b", true);
-            anim.SetFloat("Speed_f", 1);
-        }else
-        {
-            anim.SetBool("Static_b", false);
-            anim.SetFloat("Speed_f", 0);
-        }
-        player = GameObject.Find("Player");
-        enemyAgent.destination = player.transform.position;
-        if(hp <=0)
-        {
-            Destroy(gameObject);
-        }
+             //Debug.Log(enemyAgent.velocity);
+             anim.speed = 1;
+            if(enemyAgent.velocity.x != 0)
+            {
+                anim.SetBool("Static_b", true);
+                anim.SetFloat("Speed_f", 1);
+            }else
+            {
+                anim.SetBool("Static_b", false);
+                anim.SetFloat("Speed_f", 0);
+            }
+            player = GameObject.Find("Player");
+            enemyAgent.destination = player.transform.position;
+            if(hp <=0)
+            {
+                Destroy(gameObject);
+            }
+        }else{
+            anim.speed = 0;
+            enemyAgent.destination = transform.position;
+        } 
+       
     }
 
     public void TakeDamage(int damage)
